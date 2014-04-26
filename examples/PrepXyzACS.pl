294177afc426ac53594198c01e1caf77af7373d5
Demian Riccardi <demianriccardi@gmail.com>
2014-04-25 22:53:53 -0400

    set up for examples

--- examples/PrepXyzACS.pl
+++ examples/PrepXyzACS.pl
@@ -1,10 +1,15 @@
+# DMR: old school perl script that downloads a yaml and prints out some xyz
+# files
 use Modern::Perl;
 use YAML::XS qw(Dump LoadFile);
 use Path::Tiny;
 use File::chdir;
 
-#system("wget http://pubs.acs.org/doi/suppl/10.1021/ct300296k/suppl_file/ct300296k_si_001.txt");
-my $data = LoadFile("ct300296k_si_001.txt");
+my $yaml    = "ct300296k_si_001.txt";
+my $webyaml = "http://pubs.acs.org/doi/suppl/10.1021/ct300296k/suppl_file/$yaml";
+
+system("wget $webyaml") unless (-e $yaml);
+my $data = LoadFile($yaml);
 my $xyzdir = path("xyzs");
 $xyzdir->mkpath unless $xyzdir->exists;
 $CWD = $xyzdir;
